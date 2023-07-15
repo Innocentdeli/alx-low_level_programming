a1925dcc3babbc39193e669217f787e25a6e243a
innocent deli <innocentdeli1@gmail.com>
2023-07-14 21:34:25 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,21 +6,16 @@
 int main(void)
 {
 	int ones = '0';
-	int tens = '0';
-	int hundreds = '0';
-
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
-	{
+	int tens = '0';	
 		for (tens = '0'; tens <= '9'; tens++)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
+				if (!((ones == tens) || (tens > ones)))
 				{
-					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && hundreds == '7' && tens == '8'))
+					if (!(ones == '9' && tens == '8'))
 					{
 						putchar(',');
 						putchar(' ');
@@ -28,7 +23,6 @@ int main(void)
 				}
 			}
 		}
-	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
