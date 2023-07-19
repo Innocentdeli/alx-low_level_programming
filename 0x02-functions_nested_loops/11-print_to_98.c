3c997961adcb01d30b7e57be59fcc2d640374110
innocent deli <innocentdeli1@gmail.com>
2023-07-19 07:54:04 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,24 +4,23 @@
 /**
  * print_to_98 - Prints all natural numbers from n to 98.
  * @n: The number to start printing from.
- * Return: 0 (Successs)
+ * Return: return nothing
  */
 void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n > 98)
+		for (n = n; n <= 97; n++)
 		{
-			printf("%d, ", n--);
+			printf("%d, ", n);
 		}
-		printf("%d\n", n);
 	}
 	else
 	{
-		while (n < 98)
+		for (n = n; n <= 97; n--)
 		{
-			printf("%d, ", n++);
+			printf("%d, ", n);
 		}
-		printf("%d\n", n);
+		printf("98\n");
 	}
 }
