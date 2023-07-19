8d68eed45957eb09daae85e3ac5793ea01da018d
innocent deli <innocentdeli1@gmail.com>
2023-07-19 03:30:54 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,8 +12,12 @@ int print_last_digit(int n)
 
 	if (l < 0)
 	{
-		l = l * -1;
+		_putchar(-l % 48);
+		return (-l);
 	}
-	_putchar(l + 0);
-	return (l);
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
 }
