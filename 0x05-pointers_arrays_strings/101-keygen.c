4d8ac59649f90793505bbe0bd2060541b10dddfe
innocent deli <innocentdeli1@gmail.com>
2023-07-24 19:08:59 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -20,10 +20,12 @@ int main(void)
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
+		{
 			n = 2772 - sum - '0';
 			sum += n;
 			putchar(n + '0');
 			break;
+		}
 	}
 	return (0);
 }
