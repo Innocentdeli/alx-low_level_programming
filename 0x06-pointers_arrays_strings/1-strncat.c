0aab0e79bef80e78628a31e056852685cdea965e
innocent deli <innocentdeli1@gmail.com>
2023-07-26 08:38:50 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,28 @@
-#inlude "main.h"
+#include "main.h"
+/**
+ * _strncat - joins twp strings with n bytes
+ * @dest: copy to
+ * @n: number of char to be copied
+ * @src: source
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
