8a8a1a220be0513f40b543532830535c1d6c1fab
innocent deli <innocentdeli1@gmail.com>
2023-08-02 21:15:33 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,7 +17,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == '*')
 	{
-		return ((wildcmp s1 + 1, s2) || wildcmp(s1, s2 + 2));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 2));
 	}
 	else if (*s1 == *s2)
 	{
