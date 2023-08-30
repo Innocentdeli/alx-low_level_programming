2da937cdba0c76afe8ffcc34a8fb1b7bf2ed0791
innocent deli <innocentdeli1@gmail.com>
2023-08-30 07:56:53 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,11 +1,11 @@
-i#include "lists.h"
+#include "lists.h"
 
 /**
- * find_listint_loop - finds the loop
- * @head: linked list to search for
+ * find_listint_loop - finds the loop in a linked list
+ * @head: linked list to search for head
  *
- * Return: address of the node where the loop starts, or NULL
- */
+ * Return: address of the node
+ * */
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow = head;
